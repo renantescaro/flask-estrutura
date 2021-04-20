a08616d2c8106f51fb5e01c757484416fcc94c5b
renantescaro <renantescaro@yahoo.com.br>
2021-04-19 21:30:44 -0300

    implementando selects

--- daos/usuario_dao.py
+++ daos/usuario_dao.py
@@ -1,3 +1,21 @@
-class UsuarioDao:
+import json
+from daos.banco import Banco
+from entities.usuario import Usuario
+
+class UsuarioDao(Banco):
     def __init__(self):
-        pass
+        self.tabela   = 'usuario'
+        self.entidade = Usuario
+        super().__init__(
+            self.tabela,
+            self.entidade)
+
+
+    def selecionar_nomes(self):
+        return self.setar(
+            linhas = self.selecionar(campos='id, nome') )
+
+
+    def selecionar_por_id(self, id):
+        return self.setar(
+            linhas = self.selecionar(where='id='+str(id)) )
