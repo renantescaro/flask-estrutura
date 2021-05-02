c994fe7ab859a4178402c016465a011179b8f1b7
renantescaro <renantescaro@yahoo.com.br>
2021-05-02 17:05:23 -0300

    ajustes retorno em json

--- daos/banco.py
+++ daos/banco.py
@@ -1,63 +1,75 @@
 from mysql import connector
+import json
 import inspect
 import jsonpickle
 
 class Banco:
-    def __init__(self, tabela, entidade):
+    def __init__(self, tabela:str, entidade):
         self.tabela   = tabela
         self.entidade = entidade
         self.mydb     = connector.connect(
-            host     = "localhost",
-            user     = "root",
-            password = "",
-            database = "teste")
+            host      = "localhost",
+            user      = "root",
+            password  = "",
+            database  = "teste")
 
 
-    def selecionar_obj(self, campos='*', where='1=1'):
+    def selecionar_obj(self, campos:str='*', where:str='1=1'):
         mycursor = self.mydb.cursor()
         mycursor.execute(
-            ' SELECT ' + str(campos)      +
-            ' FROM '   + str(self.tabela) +
-            ' WHERE '  + str(where)       
-
-        )
-        return self.setar( mycursor.fetchall() )
+            ' SELECT ' + campos +
+            ' FROM   ' + self.tabela +
+            ' WHERE  ' + where )
+        return self.setar( mycursor.fetchall(), campos )
 
 
-    def selecionar_json(self, campos='*', where='1=1'):
+    def selecionar_json(self, campos:str='*', where:str='1=1'):
         return jsonpickle.encode(
             self.selecionar_obj(campos, where),
-            unpicklable=False)
+            unpicklable=False )
 
 
     def executar(self):
         pass
 
     
+    def editar(self, campos, where):
+        mycursor = self.mydb.cursor()
+        return mycursor.execute(
+            ' UPDATE ' + self.tabela +
+            ' SET    ' + campos +
+            ' WHERE  ' + where )
+
+    
     def deletar_tudo(self):
         return self.deletar('DELETE FROM '+str(self.tabela)+' WHERE id > 0')
             
 
-    def elemento_por_index(self, elemento, index):
+    def _elemento_por_index(self, elemento, index:int):
         if index >= len(elemento):
             return None
         else:
             return elemento[index]
 
 
-    def setar_index(self, linha, qtd_index):
+    def _setar_index(self, linha, qtd_index):
         linhas = []
         for index in range(0, qtd_index):
-            linhas.append( 
-                self.elemento_por_index(linha, index) )
+            linhas.append(
+                self._elemento_por_index(linha, index) )
         return tuple(linhas)
 
 
-    def setar(self, linhas):
-        qtd_index = (len((inspect.signature(self.entidade.__init__)).parameters) -1)
+    def setar(self, linhas:list, campos:str):
+        qtd_index = 0
+        if campos == '*':
+            qtd_index = (len((inspect.signature(self.entidade.__init__)).parameters) -1)
+        else:
+            qtd_index = len(campos.split(','))
+
         objetos = []
         for linha in linhas:
             objeto = self.entidade(
-                *self.setar_index(linha, qtd_index) )
+                *self._setar_index(linha, qtd_index) )
             objetos.append( objeto )
         return objetos
