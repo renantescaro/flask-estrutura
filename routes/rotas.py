b30e05664f00d836c566b3f3ee4c6f0318fb9bd5
renantescaro <renantescaro@yahoo.com.br>
2021-05-02 17:10:04 -0300

    adicionando inicialização de nova rota

--- routes/rotas.py
+++ routes/rotas.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response
 from routes.login_route import LoginRoute
 from routes.usuario_route import UsuarioRoute
+from routes.grupo_usuario_route import GrupoUsuarioRoute
 
 class Rotas:
     def __init__(self, app):
@@ -11,3 +12,4 @@ class Rotas:
     def _iniciar_rotas(self):
         LoginRoute(self._app)
         UsuarioRoute(self._app)
+        GrupoUsuarioRoute(self._app)
