f00598550aaae611d634cbe3620a8a482e0715e7
renantescaro <renantescaro@yahoo.com.br>
2021-05-02 17:03:49 -0300

    removendo metodos não utilizados

--- controllers/usuario_ctrl.py
+++ controllers/usuario_ctrl.py
@@ -1,26 +1,4 @@
+from daos.usuario_dao import UsuarioDao
 
 class UsuarioCtrl:
-    def usuario_atual(self):
-        pass
-
-
-    def novo(self):
-        pass
-
-
-    def editar(self):
-        pass
-
-
-    def deletar(self):
-        pass
-
-
-    def selecionar_por_id(self):
-        pass
-
-
-    def verificar_usuario_senha(self, usuario, senha):
-        if usuario == 'renan' and senha == '123':
-            return True
-        return False
+    pass
