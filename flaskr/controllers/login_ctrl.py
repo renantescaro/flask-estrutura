5c94acc6416e1b5996ea9ca6dda7097c51411f05
renantescaro <renantescaro@yahoo.com.br>
2021-05-14 16:58:26 -0300

    adicionando rotas do blueprint

--- flaskr/controllers/login_ctrl.py
+++ flaskr/controllers/login_ctrl.py
@@ -1,25 +1,34 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from flaskr.controllers.controle_acesso_ctrl import ControleAcessoCtrl
 
+bp  = Blueprint(
+    'login',
+    __name__,
+    template_folder='templates' )
+
 class LoginCtrl:
-    def inicial(self):
+    @bp.route('/')
+    def inicial():
         if 'usuario' in session:
             return render_template('inicial/inicial.html', titulo='Inicial')
         return redirect(url_for('login'))
 
 
-    def login(self):
+    @bp.route('/login')
+    def login():
         if 'usuario' in session:
             return redirect(url_for('inicial'))
         return render_template('login/login.html', titulo='Login')
 
 
-    def logout(self):
+    @bp.route('/logout')
+    def logout():
         session.clear()
         return redirect(url_for('login'))
 
 
-    def verificar_login(self):
+    @bp.route('/verificar-login')
+    def verificar_login():
         usuario = request.form.get('usuario')
         senha   = request.form.get('senha')
         if ControleAcessoCtrl().verificar_usuario_senha(usuario, senha):
