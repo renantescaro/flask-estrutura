f13c2cd57ff1cc64375c67a09fbac2873a60cfd3
renantescaro <renantescaro@yahoo.com.br>
2021-05-17 21:02:43 -0300

    select de usuario com grupos

--- flaskr/daos/usuario_dao.py
+++ flaskr/daos/usuario_dao.py
@@ -1,6 +1,7 @@
 import json
 from flaskr.daos.banco import Banco
 from flaskr.entities.usuario import Usuario
+from flaskr.utils.debug import Debug
 
 class UsuarioDao(Banco):
     def __init__(self):
@@ -8,7 +9,7 @@ class UsuarioDao(Banco):
         self.entidade = Usuario
         super().__init__(
             self.tabela,
-            self.entidade)
+            self.entidade )
 
 
     def selecionar_nomes(self):
@@ -21,5 +22,17 @@ class UsuarioDao(Banco):
             linhas = self.selecionar(where='id='+str(id)) )
 
 
-    def selecionar_com_grupo(self):
-        self.executar()
+    def selecionar_com_grupo_json(self):
+        sql = """
+            SELECT u.id, u.usuario, g.descricao AS grupo 
+            FROM usuario AS u
+            INNER JOIN usuario_grupo AS g
+            ON u.id_grupo = g.id
+            """
+        usuario_json = []
+        for usuario in self.selecionar_personalizado(sql):
+            usuario_json.append({
+                'id'     : usuario[0],
+                'usuario': usuario[1],
+                'grupo'  : usuario[2] })
+        return json.dumps(usuario_json)
