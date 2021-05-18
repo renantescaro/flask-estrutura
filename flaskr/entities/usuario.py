1634bc44fb88d226451d63e1db701f33d8d15f85
renantescaro <renantescaro@yahoo.com.br>
2021-05-17 21:03:23 -0300

    adicionando parametro grupo

--- flaskr/entities/usuario.py
+++ flaskr/entities/usuario.py
@@ -1,5 +1,6 @@
 class Usuario:
-    def __init__(self, id=None, usuario=None, senha=None):
+    def __init__(self, id=None, usuario=None, senha=None, grupo=None):
         self.id      = id
         self.usuario = usuario
         self.senha   = senha
+        self.grupo   = grupo
